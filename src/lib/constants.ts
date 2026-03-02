fef2a654d9b096c1c7909a8fb7de6bd6c5991b5f
cavvex0 <adambakiiir@gmail.com>
2026-03-03 00:13:07 +0100

    final test0

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-import HeroRight from "../assets/images/ccc.jpeg";
+// import HeroRight from "../assets/images/ccc.jpeg";
 import HeroRight2 from "../assets/images/bath4.jpeg";
 import Bath1 from "../assets/images/bbb.jpeg";
 
